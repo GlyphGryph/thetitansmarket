d119ba241ddc1bbcb329117f41754c73d198f9af
GlyphGryph <glyphgryph@gmail.com>
2014-07-27 13:02:02 -0400

    Fixed name and email saving problems, and allowed user to log in with either.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, :uniqueness => { :case_sensitive => false }, :format => { :without => /[^@]@[^@]/ }
+  attr_accessor :login
 
   def email_required?
     false
   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
