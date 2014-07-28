bf46a8cb35ca35079df139c1a8497f579288a8f3
GlyphGryph <glyphgryph@gmail.com>
2014-07-28 00:36:57 -0400

    Removed devise sql e-mail uniqueness requirement.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728041135) do
+ActiveRecord::Schema.define(version: 20140728043504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20140728041135) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "worlds", force: true do |t|
