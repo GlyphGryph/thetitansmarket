8b1272378ae9c288b236d942cc134155a85f52b7
GlyphGryph <glyphgryph@gmail.com>
2014-08-02 14:03:35 -0400

    Added rules defining and discovering valid action targets.

--- app/classes/action.rb
+++ app/classes/action.rb
@@ -1,6 +1,6 @@
 class Action
   extend CollectionTracker
-  attr_reader :id, :name, :description, :result, :cost, :available
+  attr_reader :id, :name, :description, :result, :cost, :available, :target_prompt
 
   def initialize(id, params={})
     @id = id
@@ -9,14 +9,74 @@ class Action
     @result = params[:result] || lambda { |character| return "Function error." }
     @cost = params[:cost] || lambda { |character| return 1 }
     @available = params[:available] || lambda { |character| return true }
+    @target_prompt = params[:target_prompt] || "Targeting Prompt error"
+    @targets = params[:valid_targets] || {}
     self.class.add(@id, self)
+  end
+
+  def targetable(type, id)
+    if(self.valid_targets[type] && (self.valid_target[type].include?(id) || self.valid_target[type].include?('all')))
+      return true
+    else
+      return false
+    end
+  end
+
+  # Takes a character, and returns a list of valid targets, sorted by type, that character can select for this action
+  def valid_targets(character)
+    valid = {}
+    @targets.each do |target_type, values|
+      target_objects = []
+      if(target_type == 'possession')
+        if(values.include?('all'))
+          target_objects = character.character_possessions
+        else
+          character.character_possessions.each do |character_possession|
+            if(values.include?(character_possession.possession_id))
+              target_objects << character_possession
+            end
+          end
+        end
+      elsif(target_type == 'knowledge')
+        if(values.include?('all'))
+          # Only pull from knowledges ACTUALLY known, not just those considered. You cannot consider an idea.
+          target_objects = character.knowledges
+        else
+          character.knowledges.each do |character_knowledge|
+            if(values.include?(character_knowledge.knowledge_id))
+              target_objects << character_knowledge
+            end
+          end
+        end
+      elsif(target_type == 'character')
+        if(values.include?('all'))
+          target_objects = character.world.characters
+        end
+      elsif(target_type == 'condition')
+        if(values.include?('all'))
+          target_objects = character.character_conditions
+        else
+          character.character_conditions.each do |character_conditions|
+            if(values.include?(character_condition.condition_id))
+              target_objects << character_condition
+            end
+          end
+        end
+      else
+        raise "Invalid target type for Action: valid targets"
+      end
+      valid[target_type]=target_objects
+    end
   end
 end
 
 # Format for new actions
 # 'id', {:name => 'Name', :description=>"Multi-word description.", 
 # :result => lambda {|character| return "Some string based on what happens, possibly conditional on character state" }, // Takes a character, returns a string
-# :cost => lambda {|character| if(character.loves_chicken) return 5; else return 6; } // Takes a character, returns a digit
+# :cost => lambda {|character| if(character.loves_chicken) return 5; else return 6;} // Takes a character, returns a digit
+# :available => lambda {|character| return true or false} // Whether or not the player can currently do this action
+# :valid_targets => {'type_name' => ['id', 'id']} // Types are possession, knowledge, condition, character. 'all' can be used in place of an id to indicate that every object of that type is a valid target
+# }
 
 Action.new("forage",
   { :name=>"Forage", 
@@ -52,7 +112,9 @@ Action.new("ponder",
     :cost => lambda { |character| return 3 },
     :available => lambda { |character|
       return character.knows?("cognition") && !character.knows?("basic_farming") && !character.considers?("basic_farming")
-    }
+    },
+    :valid_targets => {'possession'=>['all'], 'condition'=>['all'], 'knowledge'=>['all'], 'character'=>['all']},
+    :target_prompt => "What would you like to ponder?",
   }
 )
 Action.new("investigate",
