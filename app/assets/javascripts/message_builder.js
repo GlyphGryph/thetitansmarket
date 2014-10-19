918460e6efa9b5fbccb7936d2351b254703b60b3
GlyphGryph <glyphgryph@gmail.com>
2014-10-19 15:42:22 -0400

    Finished the front end basic interface for message building, now sends the appropriate package to the backend.

--- app/assets/javascripts/message_builder.js
+++ app/assets/javascripts/message_builder.js
@@ -1,26 +1,28 @@
-var foo = null;
 jQuery("document").ready(function(){
+  var next_component_id = 0;
   var can_make_new = true;
-  jQuery(".message-form .add-gesture").click(function(){
+  jQuery(".message-details .add-gesture").click(function(){
     if(can_make_new && disableNew()){
       var template_content = jQuery(".reference-template.new-gesture").html();
       var new_element = jQuery(template_content);
       template_content = jQuery(".reference-template.select-gesture").html();
       new_element.find(".gesture-builder").html(template_content);
       jQuery(".message-list").append(new_element);
-      foo = new_element;
       new_element.find(".gesture-option").click(selectGestureOption);
     }
   });
 
-  jQuery(".message-form .add-speech").click(function(){
+  jQuery(".message-details .add-speech").click(function(){
     var template_content = jQuery(".new-speech").html();
-    var new_element = jQuery("<div class='message-element speech-element'>"+template_content+"</div>");
+    var new_element = jQuery(template_content);
     jQuery(".message-list").append(new_element);
+    new_element.find(".type-field").attr("name", "message_components["+next_component_id+"][type]");
+    new_element.find(".text-field").attr("name", "message_components["+next_component_id+"][value]");
+    next_component_id+=1;
   });
 
   function disableNew(){
-    jQuery(".message-form-controls").hide();
+    jQuery(".message-details-controls").hide();
     if(can_make_new){
       can_make_new = false;
       return true;
@@ -30,17 +32,28 @@ jQuery("document").ready(function(){
   };
 
   function enableNew(){
-    jQuery(".message-form-controls").show();
+    jQuery(".message-details-controls").show();
     can_make_new = true;
     return true;
   };
 
   function selectGestureOption(){
     var selected = jQuery(this);
-    foo = selected;
-    var gesture_element = selected.parents(".message-element");
-    gesture_element.text(selected.find(".option-description").text());
+    var selected_id = selected.data("id");
+    var selected_text = selected.find(".option-description").text();
+    var template_content = jQuery(".reference-template.commit-gesture").html();
+    var gesture_element = selected.parents(".message-element")
+    gesture_element.html(template_content);
+    gesture_element.find(".id-field").val(selected_id);
+    gesture_element.find(".id-field").attr("name", "message_components["+next_component_id+"][id]");
+    gesture_element.find(".type-field").attr("name", "message_components["+next_component_id+"][value]");
+    gesture_element.find(".description").text(selected_text);
+    next_component_id+=1;
     enableNew();
   };
 });
 
+/*
+<input value="wave" class="gesture-field" id="message_components_gestures_" name="message_components[gestures][]" type="hidden">
+<textarea id="message_components_speeches_" name="message_components[speeches][]"></textarea>
+*/
