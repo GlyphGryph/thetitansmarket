0880ae38edfb424a018d374c9607760cf8a5293f
GlyphGryph <glyphgryph@gmail.com>
2014-11-04 21:50:26 -0500

    Added a few more gestures

--- lib/data/gesture/all.rb
+++ lib/data/gesture/all.rb
@@ -55,3 +55,96 @@ Gesture.new("point",
     :valid_targets => {:possession=>['all'], :character=>['all'], :body=>['all']},
   }
 )
+
+Gesture.new("wink",
+  { :name => "Wink", 
+    :description => "Wink at someone.", 
+    
+    :outcomes => {
+      :viewer_is => {
+        :actor_and_owner => {
+          :owner_is_target => "You wink at yourself.",
+          :owner_is_not_target => "You wink at your %{target}",
+        },
+        :actor => {
+          :owner_is_target => "You wink at %{target}",
+          :owner_is_not_target => "You wink at %{owned} %{target}",
+        },
+        :owner => {
+          :owner_is_target => "%{actor} winks at you.",
+          :owner_is_not_target => "%{actor} winks at your %{target}",
+        },
+        :nothing => {
+          :owner_is_target => "%{actor} winkss at %{target}",
+          :owner_is_not_target => "%{actor} winks at %{owned} %{target}",
+        }
+      },
+    },
+
+    :target_prompt => "Who would you like to wink at?",
+    :requires_target => true,
+    :valid_targets => {:character=>['all']},
+  }
+)
+
+Gesture.new("hug",
+  { :name => "Hug", 
+    :description => "Hug someone.", 
+    
+    :outcomes => {
+      :viewer_is => {
+        :actor_and_owner => {
+          :owner_is_target => "You hug yourself.",
+          :owner_is_not_target => "You hug your %{target}",
+        },
+        :actor => {
+          :owner_is_target => "You hug %{target}",
+          :owner_is_not_target => "You hug %{owned} %{target}",
+        },
+        :owner => {
+          :owner_is_target => "%{actor} hugs at you.",
+          :owner_is_not_target => "%{actor} hugs your %{target}",
+        },
+        :nothing => {
+          :owner_is_target => "%{actor} hugs %{target}",
+          :owner_is_not_target => "%{actor} hugs %{owned} %{target}",
+        }
+      },
+    },
+
+    :target_prompt => "Who would you like to wink at?",
+    :requires_target => true,
+    :valid_targets => {:character=>['all']},
+  }
+)
+
+Gesture.new("gnaw",
+  { :name => "Gnaw", 
+    :description => "Gnaw on something.", 
+    
+    :outcomes => {
+      :viewer_is => {
+        :actor_and_owner => {
+          :owner_is_target => "You gnaw on yourself.",
+          :owner_is_not_target => "You gnaw on your %{target}",
+        },
+        :actor => {
+          :owner_is_target => "You gnaw on %{target}",
+          :owner_is_not_target => "You gnaw on %{owned} %{target}",
+        },
+        :owner => {
+          :owner_is_target => "%{actor} gnaw on you.",
+          :owner_is_not_target => "%{actor} gnaw on your %{target}",
+        },
+        :nothing => {
+          :owner_is_target => "%{actor} gnaw on %{target}",
+          :owner_is_not_target => "%{actor} gnaws on %{owned} %{target}",
+        }
+      },
+    },
+
+    :target_prompt => "What would you like to gnaw on?",
+    :requires_target => true,
+    :valid_targets => {:possession=>['all'], :character=>['all'], :body=>['all']},
+  }
+)
